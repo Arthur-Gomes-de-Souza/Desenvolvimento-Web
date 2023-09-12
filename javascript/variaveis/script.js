7fc6ccc3b15e619fab4acee6ad82479459a1b789
Arthur Gomes de Souza <126431991+Arthur-Gomes-de-Souza@users.noreply.github.com>
2023-09-11 21:50:55 -0300

    First commit

---
+++ javascript/variaveis/script.js
@@ -0,0 +1,15 @@
+// STRING:
+var texto = 'Curso de JavaScript'
+
+// NUMEROS:
+var numInteiro = -7
+var numFracionario = 11.75
+
+// BOOLEAN:
+var teste = true
+
+alert(numFracionario) // ABRIR UM DIALOGO
+
+document.write(texto)
+
+console.log(numInteiro) // RESULTADO NO CONSOLE (CTRL + SHIFT + I)
